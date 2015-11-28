49c54fd9d784d99a2ebc13d42610668e3bf68d84
dione <dione@DESKTOP-CV7IBAF>
2015-11-27 23:26:26 -0300

    commit da classe evento

---
+++ src/alocador/classes/Evento.java
@@ -0,0 +1,11 @@
+
+package alocador.classes;
+
+public class Evento {
+    protected int horarioInicial;
+    protected int horarioFinal;
+    protected String nome;
+    protected String responsavel;
+    protected Sala salaUtilizada;
+    protected Semestre semestre;
+}
